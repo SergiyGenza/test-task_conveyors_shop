8fc8de32d2820fb33996e34885fe62f38195fb79
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-03-03 22:10:22 +0200

    feat: add custom cheboxes

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ const previewImg = document.querySelector("#preview");
 const prevButtons = document.querySelectorAll(".btn-slide.prev");
 const nextButtons = document.querySelectorAll(".btn-slide.next");
 const slider = document.querySelectorAll(".slider-item img");
-
+const options = document.querySelectorAll(".options-group");
 let currentIndex = 0;
 
+// slider
 function showSlide(index) {
   previewImg.src = sliderItems[index];
   setActiveSlide();
+}
+
+function setActiveSlide() {
+  slider.forEach((slide) => {
+    slide.classList.remove("active-slide");
+    if (slide.id == currentIndex) {
+      slide.classList.add("active-slide");
+    }
+  });
 }
 
 prevButtons.forEach((prevButton) => {
@@ -32,6 +42,7 @@ nextButtons.forEach((nextButton) => {
   });
 });
 
+// full screen
 function full_view() {
   let src = previewImg.getAttribute("src");
   document
@@ -41,17 +52,44 @@ function full_view() {
   document.querySelector("#img-viewer").style.display = "block";
 }
 
-function close_model() {
+function closeModal() {
   document.querySelector("#img-viewer").style.display = "none";
 }
 
-function setActiveSlide() {
-  slider.forEach((slide) => {
-    slide.classList.remove("active-slide");
-    if (slide.id == currentIndex) {
-      slide.classList.add("active-slide");
+// product info
+
+const buttons = document.querySelectorAll(".options-group .btn-option");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const isActive = this.classList.contains("btn-option-active");
+    buttons.forEach((btn) => {
+      btn.classList.remove("btn-option-active");
+    });
+    if (!isActive) {
+      this.classList.add("btn-option-active");
     }
   });
+});
+
+function onCustomSizeOpen() {
+  const customSize = document.querySelector("#product-width");
+  console.log(customSize);
+  customSize.classList.toggle("open");
 }
 
 showSlide(currentIndex);
+
+// custom checkboxes
+var checkboxes = document.querySelectorAll(
+  '.product-options-group input[type="checkbox"]'
+);
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      console.log("Checkbox with ID", this.id, "is checked.");
+    } else {
+      console.log("Checkbox with ID", this.id, "is unchecked.");
+    }
+  });
+});
